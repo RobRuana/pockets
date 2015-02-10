32783a48141a29b673681bba87a92489278c1469
Rob Ruana <rob@robruana.com>
2015-02-10 10:16:09 -0500

    Rename README.rst to README.md in files that expect it to exist

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author='Rob Ruana',
     author_email='rob@robruana.com',
     description=__doc__,
-    long_description=open('README.rst', 'r').read(),
+    long_description=open('README.md', 'r').read(),
     zip_safe=True,
     classifiers=[
         'Development Status :: 4 - Beta',
