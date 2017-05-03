8acb198229eb0fad3294f44597252b1a508858bc
Rob Ruana <rob@robruana.com>
2017-05-03 11:26:31 -0400

    Updates copyright in docs/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,7 +68,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pockets'
-copyright = u'2016 Rob Ruana'
+copyright = u'2017 The Pockets Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
